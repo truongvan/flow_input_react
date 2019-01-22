ec46c0d8aa98c58847fc9b285674b8f6120ca163
truongvan <truongvan@live.com>
2019-01-22 16:00:19 +0700

    add project

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,6 @@
 import React, { Component } from 'react'
 import { Route } from "react-router-dom"
 
-import Landingpage from './components/landingpage/Landingpage'
-import Dashboard from './components/dashboard/Dashboard'
-import Page from './components/page/Page'
-import Search from './components/search/Search'
 import Register from './components/register/Register'
 
 // const Hoc = (props) => props.children
@@ -14,11 +10,7 @@ export default class BaseRouter extends Component {
     render() {
         return (
             <div>
-                <Route exact path="/" component={Landingpage} />
-                <Route exact path="/page/:slug([a-z0-9\-]+)" component={Page} />
-                <Route exact path="/search" component={Search} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/dashboard/" component={Dashboard} />
+                <Route exact path="/" component={Register} />
             </div>
         )
     }
